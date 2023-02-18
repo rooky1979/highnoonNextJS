87937c8d622d4d68a547c5e08858911aaecf31e4
rooky1979 <74156579+rooky1979@users.noreply.github.com>
2023-02-18 20:42:25 +1300

    Adding image to nav bar

--- components/nav.js
+++ components/nav.js
@@ -1,6 +1,12 @@
 import styles from "@/styles/Home.module.css";
+import Image from 'next/image'
 
 const Nav = () => {
-    return <p className={styles.navbar}>Welcome to the High Noon Gamble</p>
+    return     <Image
+    src="/HNGlong.png"
+    alt="Picture of the author"
+    width={500}
+    height={200}
+  />
 };
 export default Nav;
--- pages/preamble.js
+++ pages/preamble.js
@@ -1,28 +1,28 @@
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
-import useSWR from 'swr';
+import useSWR from "swr";
 
 const inter = Inter({ subsets: ["latin"] });
-const fetcher = (url) => fetch(url).then((res) =>res.json());
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function preamble() {
+  const { data, error } = useSWR("/api/staticdata", fetcher);
 
-  const {data, error} = useSWR('/api/staticdata', fetcher)
+  if (error) return <div>Failed to load</div>;
+  if (!data) return <div>Loading....</div>;
 
-  if(error) return <div>Failed to load</div>
-  if(!data) return <div>Loading....</div>
-
-  return (//UP TO HERE. TRYING TO DRAG DATA FROM THE PREAMBLE JS FILE
+  const preambleText = JSON.parse(data);
+  return (
+    //UP TO HERE. TRYING TO DRAG DATA FROM THE PREAMBLE JS FILE
     <>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <h1 className={styles.title}>Preamble</h1>
-          <p>
-            {data.preamble.text1}
-        </p>
-        </div>
-        
+      <main className={styles.textpanel}>
+      <h1 className={styles.title}>Preamble</h1>
+        <div>
+          <p className={styles.readingtext}>{preambleText.preamble.text1}
+          <br></br><br></br>
+          {preambleText.preamble.text2}</p>
 
+        </div>
       </main>
     </>
   );
