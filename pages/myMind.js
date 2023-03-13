6cf4279af9985e1472881db14775d85d057eb9d0
rooky1979 <74156579+rooky1979@users.noreply.github.com>
2023-03-13 19:25:59 +1300

    added search function

--- pages/myMind.js
+++ pages/myMind.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 import { mymind } from "../json/mymind";
@@ -5,6 +6,25 @@ import { mymind } from "../json/mymind";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function myMind() {
+
+  const [allData, setAllData] = useState(mymind);
+
+  useEffect(() => {
+    setAllData(mymind);
+  },[])
+
+  const [filteredData, setFilteredData] = useState(allData);
+
+  const handleSearch = (e) => {
+    let value = e.target.value.toLowerCase();
+    let result = [];
+    result = allData.filter((data) => {
+      return data.id.search(value) != -1;
+    });
+  
+    setFilteredData(result);
+  };
+
   return (
     <>
       <main className={styles.readingsmain}>
@@ -18,13 +38,14 @@ export default function myMind() {
                 id="mymind"
                 name="mymind"
                 placeholder="Enter a number from 1 - 526"
+                onChange={(e) => handleSearch(e)}
               />
               <button className={styles.btn} onClick="">
                 Deal
               </button>
             </form>
-            {mymind.map((saying) => (
-              <div className={styles.quotecard}>
+            {filteredData?.map((saying, index) => (
+              <div className={styles.quotecard} key={saying.id}>
                 <div className={styles.readingtext}>
                   {saying.id}: {saying.quote}
                 </div>
