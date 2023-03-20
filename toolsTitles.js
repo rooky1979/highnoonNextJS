f3ebd88ffc1d69a062418627c63aeb9bc4ea4f46
rooky1979 <74156579+rooky1979@users.noreply.github.com>
2023-03-21 10:37:16 +1300

    fixes

--- toolsTitles.js
+++ toolsTitles.js
@@ -3,21 +3,21 @@ export const tools = [
     id: "1",
     book: "My Mind Is Out To Get Me:",
     tagline: "Humour and Wisdom in Recovery",
-    ref: "/MyMind",
+    ref: "/myMind",
     src: "mindout.jpg",
   },
   {
     id: '2',
     book: '101 Common Cliches of Alcoholics Anonymous:',
     tagline: 'The Sayings the Newcomers Hate and the Oldtimers Love',
-    ref: "/CommonCliches",
+    ref: "/commonCliches",
     src: "101cliches.jpg",
   },
   {
     id: '3',
     book: '101 Additional Common Cliches of Alcoholics Anonymous:',
     tagline: "More Sayings the Newcomers Hate and the Oldtimers Love.",
-    ref: "/AdditionalCliches",
+    ref: "/additionalCliches",
     src: "101additional.jpg",
   },
   {
@@ -31,7 +31,7 @@ export const tools = [
     id: "5",
     book: "Big Book Cards:",
     tagline: "52 taglines from AA's Big Book",
-    ref: "/BbCards",
+    ref: "/bbCards",
     src: "bbcards.png",
   },
   {
