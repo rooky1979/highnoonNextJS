9795bf26f6c6f2976667ad469b17bb5c1b620db5
rooky1979 <74156579+rooky1979@users.noreply.github.com>
2023-03-24 18:15:26 +1300

    spinner

--- pages/chapter3.js
+++ pages/chapter3.js
@@ -9,7 +9,7 @@ const Chapter3 = () => {
   const { data, error } = useSWR("/api/chapter3API", fetcher);
 
   if (error) return <div>Failed to load</div>;
-  if (!data) return <Spinner/>;
+  if (!data) return <div class="loader"></div>;
 
   const chapter3Text = JSON.parse(data);
   return (
