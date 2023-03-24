a3d9f5c742fb53202ec8d17f99990e42fbe81871
rooky1979 <74156579+rooky1979@users.noreply.github.com>
2023-03-24 18:07:11 +1300

    spinner

--- components/spinner.js
+++ components/spinner.js
@@ -1,5 +1,7 @@
 import Image from "next/image";
 
 export default function Spinner() {
-  return <Image src="/spinner.gif" alt="loading..." width="100" height="100" />;
+  return  <div className={styles.card}>
+  <Image src="/spinner.gif" alt="loading..." width="100" height="100" />;
+  </div>
 }
