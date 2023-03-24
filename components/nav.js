11189eebfb616e34716310b588fcd5979a23ee40
rooky1979 <74156579+rooky1979@users.noreply.github.com>
2023-03-24 18:34:16 +1300

    fixes

--- components/nav.js
+++ components/nav.js
@@ -1,5 +1,4 @@
-import styles from "@/styles/Home.module.css";
-import Image from 'next/image'
+import styles from "../styles/Home.module.css";
 
 const Nav = () => {
     return <div className={styles.navbar}>
